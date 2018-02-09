733a215a966a765fc5438da6dfae13d72e4dd35e
itggot-max-gustafsson <itggot-max-gustafsson@users.noreply.github.com>
2018-02-09 13:59:40 +0100

    massivt krångel med allt

---
+++ site/js/javascript.js
@@ -0,0 +1,6 @@
+function dropdown() {
+    var element1 = document.querySelector(".container");
+    var element2 = document.querySelector(".dropdowncontainer");
+    element1.classList.toggle = "show"; // fixa detta som en klass som visar
+    element2.classList.toggle = "unshow"; //detta med
+}
